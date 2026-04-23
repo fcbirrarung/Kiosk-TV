e9420c0da5157daa3e8e4b54c22a44b2ddc2da30
FC Birraurng <president@fcbirrarung.com>
2026-04-23 17:16:54 +1000

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js -- DO NOT COMMIT (listed in .gitignore)
-const CONFIG = {
-  API_KEY:       'AIzaSyDHZdTYxjrtjEmi1xd4C-t3M3cbVFiZkxQ',
-  FOLDER_ID_TV1: 'Canteen',
-  FOLDER_ID_TV2: 'Canteen Outdoor TV',
-  FOLDER_ID_TV3: 'Kit Room Outdoor TV',
-};
