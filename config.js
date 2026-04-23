739bc73bc869654152370c57e6e49c064d68fa85
FC Birraurng <president@fcbirrarung.com>
2026-04-24 08:11:05 +1000

    Rotation to 5 seconds

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     3: 'landscape',
   },
 
-  IMAGE_DURATION_SEC:  4,
+  IMAGE_DURATION_SEC:  5,
   PLAYLIST_REFRESH_MS: 5 * 60 * 1000,
 
   IMAGE_TYPES: ['image/jpeg', 'image/png', 'image/gif', 'image/webp'],
