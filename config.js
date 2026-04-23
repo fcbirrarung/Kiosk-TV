44e1c567c0e57f312ddde69bf4e3cedf031096e7
FC Birraurng <president@fcbirrarung.com>
2026-04-23 19:09:10 +1000

    Orientation option

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ const CONFIG = {
   // Cloudflare Worker URL (deployed via: npx wrangler deploy)
   WORKER_URL: 'https://kiosk-playlist.president-114.workers.dev',
 
+  // Orientation per TV: 'portrait' (9:16) or 'landscape' (16:9)
+  ORIENTATIONS: {
+    1: 'portrait',
+    2: 'portrait',
+    3: 'landscape',
+  },
+
   IMAGE_DURATION_SEC:  4,
   PLAYLIST_REFRESH_MS: 5 * 60 * 1000,
 
